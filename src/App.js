1ab02751c12b093450858b3bb2cd4943ac7a56ed
“KseGreb” <ksegreb@gmail.com>
2023-11-03 12:41:29 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import { useMemo, useState } from 'react';
 import './App.css';
 
+const fruits = [
+  {id: "1", item: "Peach"}, 
+  {id: "2", item: "Plum"},
+  {id: "3", item: "Apple"},
+  {id: "4", item: "Banana"},
+  {id: "5", item: "Pineapple"}
+];
+
 function App() {
+
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleSearch = () =>{
+    setSearch(text);
+  }
+
+  const handleText = (e) =>{
+    setText(e.target.value)
+  }
+
+  const filteredFruits = useMemo( ()=> fruits.filter((fruit) => {
+      return fruit.item.toLowerCase().includes(search.toLowerCase());
+    }), [search])
+
+//without using useMemo hook this component will be rendering too much and performance will not be great: 
+  // const filteredFruits = fruits.filter((fruit) => {
+  //  console.log("Rendering!!!")
+  //  return fruit.item.toLowerCase().includes(search.toLowerCase());
+  // })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Filtering...</h1>
+      <input type="text" onChange={handleText}></input>
+      <button type="button" onClick={handleSearch}>Search</button>
+      <div>
+        {filteredFruits.map((filteredFruit) =>(
+          <p key={filteredFruit.id}>{filteredFruit.item}</p>
+          ))}
+      </div>
     </div>
   );
 }
